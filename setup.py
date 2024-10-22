acdca1dd6af42a8914d1b0f954500747de8b3cd9
Riddhick <riddhick14@gmail.com>
2024-10-22 23:07:42 +0530

    Version Change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="neuronflow",
-    version="0.0.1",
+    version="0.0.2",
     long_description=long_description,
     long_description_content_type='text/markdown',
     #description="A lightweight machine learning library",
