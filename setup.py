6d928410f3b53d5602662d8ee15a21f599ab54e1
Riddhick <riddhick14@gmail.com>
2024-10-22 11:46:49 +0530

    Version Updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages,setup
 
 setup(
     name="neuronflow",
-    version="0.1.0",
+    version="0.0.1",
     description="A lightweight machine learning library",
     author="Riddhick Dalal",
     author_email="riddhick14@gmail.com",
