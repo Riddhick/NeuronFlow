f3239da9af67756f3f140c314f07d71dbe8b96a1
Riddhick <riddhick14@gmail.com>
2024-10-24 21:34:40 +0530

    Update : Polynomial Regression ⬆️

--- neuronflow/regression.py
+++ neuronflow/regression.py
@@ -17,12 +17,14 @@ class linear:
         thetas=np.matmul(temp,self.Y)
         self.thetas=thetas
         return self.thetas
+    
     def value(self,x):
         x=x.reshape(x.shape[0],1)
         ones=np.ones(x.shape[0]).reshape(x.shape[0],1)
         x=np.append(ones,x,axis=1)
         values=np.matmul(x,self.thetas)
         return values.T
+    
     def r_square(self,x,y):
         y=y.reshape(y.shape[0],1).T
         y_pred=self.value(x)
@@ -45,11 +47,13 @@ class multilinear(linear):
         thetas=super().fit()
         self.thetas=thetas
         return self.thetas
+    
     def value(self,x):
         ones=np.ones(x.shape[0]).reshape(x.shape[0],1)
         x=np.append(ones,x,axis=1)
         values=np.matmul(x,self.thetas)
         return values.T
+    
     def r_square(self,x,y):
         y=y.reshape(y.shape[0],1).T
         y_pred=self.value(x.T)
@@ -71,17 +75,21 @@ class polynomial(linear):
         self.X=self.X**np.arange(0,self.X.shape[1])
        
     def fit(self):
-        xTranspose=self.X.T
-        X_t_X=np.matmul(xTranspose,self.X)
-        if(np.linalg.det(X_t_X)==0):
-            X_inv=np.linalg.pinv(X_t_X)
-        else:
-            X_inv=np.linalg.inv(X_t_X)
-        temp=np.matmul(X_inv,xTranspose)
-        thetas=np.matmul(temp,self.Y)
+        thetas=super().fit()
         self.thetas=thetas
         return self.thetas
     
-    def values(self,x):
-        pass
+    def value(self,x):
+        degree=self.thetas.shape[0]-1
+        x=x.reshape(x.shape[0],1)
+        ones=np.ones(x.shape[0]).reshape(x.shape[0],1)
+        x=np.append(ones,x,axis=1)
+        x=np.repeat(x,[1,degree],axis=1)
+        x=x**np.arange(0,x.shape[1])
+        values=np.matmul(x,self.thetas)
+        return values.T
+    
+    def r_square(self, x, y):
+        return super().r_square(x, y)
+        
 
